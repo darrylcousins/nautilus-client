1006e463c0ae9cb6220672d83c3cdc4c5e0f3054
darrylcousins <darryljcousins@gmail.com>
2019-01-06 19:39:01 +1300

    Bring in tachyons.css for styles and fontawesome for svg icons

--- docs/install.rst
+++ docs/install.rst
@@ -10,62 +10,46 @@ Create a react starter app::
 
 Maybe it should all work by cloning this repository and then continue.
 
-Python virtual environment::
-
-  $ cd nautilus
-  $ virtualenv -p python3 .
-  $ source bin/activate
-
-Install `nodeenv` into same directory and activate::
-
-  $ pip install nodeenv
-  $ nodeenv -p
-  $ source bin/activate
-
-In order to satisfy Typescript I added a `dummy.ts` file and only then could run::
-
-   (nautilus) $ npm start
-
 Additional Components
 ---------------------
 
 With local `npm` install `react-router`::
 
-  (nautilus) $ npm install --save react-router react-router-dom
+  $ npm install --save react-router react-router-dom
 
 Install `react`/`apollo`/`graphql` stack::
 
-  (nautilus) $ npm install --save apollo-boost apollo-link-context react-apollo graphql-tag graphql
+  $ npm install --save apollo-boost apollo-link-context react-apollo graphql-tag graphql
 
 `react-form`::
 
-  (nautilus) $ npm install --save react-form
+  $ npm install --save react-form
 
 `font-awesome`::
 
-  (nautilus) $ npm install --save @fortawesome/fontawesome
-  (nautilus) $ npm install --save @fortawesome/fontawesome-svg-core
-  (nautilus) $ npm install --save @fortawesome/react-fontawesome
-  (nautilus) $ npm install --save @fortawesome/fontawesome-free-solid
-  (nautilus) $ npm install --save @fortawesome/fontawesome-free-regular
-  (nautilus) $ npm install --save @fortawesome/fontawesome-free-brands
+  $ npm install --save @fortawesome/fontawesome
+  $ npm install --save @fortawesome/fontawesome-svg-core
+  $ npm install --save @fortawesome/react-fontawesome
+  $ npm install --save @fortawesome/fontawesome-free-solid
+  $ npm install --save @fortawesome/fontawesome-free-regular
+  $ npm install --save @fortawesome/fontawesome-free-brands
 
 Using `PapaParse <https://www.papaparse.com/>`_ to handle csv files::
 
-  (nautilus) $ npm install --save papaparse
+  $ npm install --save papaparse
 
 Using `react-table <https://react-table.js.org>`_ to display data.::
 
-  (nautilus) $ npm install --save react-table
+  $ npm install --save react-table
 
 Transitions::
 
-  (nautilus) $ npm install --save react-transition-group
+  $ npm install --save react-transition-group
 
 CSV helper::
 
-  (nautilus) $ npm install --save react-csv
+  $ npm install --save react-csv
 
 Modals::
 
-  (nautilus) $ npm install --save react-modal
+  $ npm install --save react-modal
--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ import {
   Route,
 } from 'react-router-dom'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import faUsers from '@fortawesome/fontawesome-free-solid/faUsers'
+import faGithub from '@fortawesome/fontawesome-free-brands/faGithub'
+
 import Client from './client'
 import Index from './components/index'
 import GlossaryEntries from './components/glossary-entries'
@@ -22,10 +26,42 @@ class App extends Component {
     return (
       <ApolloProvider client={ Client }>
         <Router>
-          <div>
-            <Route exact path="/" component={ Index } />
-            <Route exact path="/glossary" component={ GlossaryEntries } />
-            <Route exact path="/diary" component={ DiaryEntries } />
+          <div className="w-100 sans-serif">
+            <div className="center w85">
+              <header>
+                <div className="w-100 pa2 ph2-ns pt3-ns pb1 bg-white">
+                  <div className="db dt-ns mw9 center w-100">
+                    <div className="db dtc-ns v-mid tl w-50">
+                      <Link to="/" className="f5 f4-ns fw6 mt0 mb1 link black-70 dib" title="Home">
+                        Nautilus
+                        <div className="dib">
+                          <small className="nowrap f6 ml1 mt2 mt3-ns pr2 black-70 fw2">v0.1.0</small>
+                        </div>
+                      </Link>
+                    </div>
+                    <nav className="db dtc-ns v-mid w-100 tl tr-ns mt2 mt0-ns">
+                      <a title="Nautilus on GitHub"
+                        href="https://github.com/darrylcousins/nautilus-client"
+                        className="link dim f6 fw6 link navy mr2 mr3-m mr4-l dib pointer">
+                        <FontAwesomeIcon icon={ faGithub } color="navy" />
+                        &nbsp;GitHub
+                      </a>
+                    </nav>
+                  </div>
+                </div>
+              </header>
+              <div className="ph2-ns">
+                <div className="cf mw9 tc-m">
+                  <div className="pb2 pb3-ns pt2 mt0 black-70">
+                    <div className="ph1 pv1 background-gray tl">
+                      <Route exact path="/" component={ Index } />
+                      <Route exact path="/glossary" component={ GlossaryEntries } />
+                      <Route exact path="/diary" component={ DiaryEntries } />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </Router>
       </ApolloProvider>
